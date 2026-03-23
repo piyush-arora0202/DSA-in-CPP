14f003a080169d0ee3c288369c649ec4b4b59b69
Piyush Arora <your_github_email@gmail,com>
2026-03-23 15:43:37 +0530

    Added Backtracking file

---
+++ Backtracking/subsets.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+class Solution {
+public:
+    void getAllSubsets(vector<int> &nums, vector<int> &ans,int i,vector<vector<int>> &allSubsets) {
+        if (i == nums.size()) {
+            allSubsets.push_back(ans);
+            return;
+        }
+        // include
+        ans.push_back(nums[i]);
+        getAllSubsets(nums,ans,i+1,allSubsets);
+        // exclude
+        ans.pop_back();
+        getAllSubsets(nums,ans,i+1,allSubsets);        
+    }
+
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> allSubsets;
+        vector<int> ans;
+
+        getAllSubsets(nums,ans,0,allSubsets);
+        return allSubsets;
+    }
+};
