6496086772f2a461f48e6703f8b9e5b79c15b334
Piyush Arora <your_github_email@gmail,com>
2026-04-12 15:35:25 +0530

    Added new files

---
+++ Queue/First_Unique_Character_String.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <unordered_map>   
+using namespace std;
+
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char,int>m;
+        queue<int> q;
+
+        for (int i=0;i<s.size();i++) {
+            if (m.find(s[i]) == m.end()){
+                q.push(i);
+            }
+            m[s[i]]++;
+            while (q.size() > 0 && m[s[q.front()]] > 1) {
+                q.pop();
+            }
+        }
+        return q.empty() ? -1 : q.front();
+
+    }
+};
